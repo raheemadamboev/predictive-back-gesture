cb1eec830cefdf8b7bdd36016d17a4b46dd845b2
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-08 13:25:28 +0500

    install dependencies

--- app/src/main/java/xyz/teamgravity/predectivebackgesture/Main.kt
+++ app/src/main/java/xyz/teamgravity/predectivebackgesture/Main.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import xyz.teamgravity.predectivebackgesture.ui.theme.PredectiveBackGestureTheme
 
-class MainActivity : ComponentActivity() {
+class Main : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
