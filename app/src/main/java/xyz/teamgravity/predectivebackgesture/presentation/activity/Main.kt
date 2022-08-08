5f0b0cc8ac06486c2c8420d8cf9a4037e27675d8
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-08 14:25:19 +0500

    add back handler

--- app/src/main/java/xyz/teamgravity/predectivebackgesture/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/predectivebackgesture/presentation/activity/Main.kt
@@ -2,14 +2,27 @@ package xyz.teamgravity.predectivebackgesture.presentation.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import xyz.teamgravity.predectivebackgesture.R
 import xyz.teamgravity.predectivebackgesture.presentation.theme.PredectiveBackGestureTheme
 
 class Main : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -18,7 +31,27 @@ class Main : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val dispatcher = LocalOnBackPressedDispatcherOwner.current!!.onBackPressedDispatcher
+                    var countdown by remember { mutableStateOf(3) }
 
+                    BackHandler(enabled = countdown != 0) {
+                        countdown--
+                    }
+
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.your_press_count, countdown),
+                            modifier = Modifier.padding(horizontal = 10.dp)
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Button(onClick = { dispatcher.onBackPressed() }) {
+                            Text(text = stringResource(id = R.string.go_back))
+                        }
+                    }
                 }
             }
         }
